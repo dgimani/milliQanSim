9e44ee7e385423c5e0b84d7002d5c093be35689e
ryan <schmitz@ucsb.edu>
2023-04-05 14:28:17 -0700

    Updated a simple plotting script with example for how to use sim shared library to load in dictionary

--- rootMacros/plotMacros/MuonPMTHit.C
+++ rootMacros/plotMacros/MuonPMTHit.C
@@ -1,17 +1,19 @@
 #include "TCanvas.h"
 #include "TTree.h"
 #include "TFile.h"
+R__LOAD_LIBRARY(../../build/libBenchCore.so)
 
 void MuonPMTHit(){
 
   TChain ch("Events");
-   ch.Add("/media/ryan/Storage/computing/mqTestStand/data/Test/MilliQan.root");
+   ch.Add("/home/schmitz/milliQanSim/build/Sim_1125852016MilliQan.root"); //some test sim output file
 
-  TH1F *hMEDep = new TH1F("hMECount","Photoelectrons Detected Per Incident Muon, Test",100,1000,1500);
-  hMECount->GetXaxis()->SetTitle("Number of Photoelectrons Detected");
-  hMECount->GetYaxis()->SetTitle("Bin Members");
+  TH1F *hNPE = new TH1F("hNPE","Photoelectrons Detected Per Incident Gamma, Test",50,0,50); //
+  hNPE->GetXaxis()->SetTitle("Number of Photoelectrons Detected");
+  hNPE->GetYaxis()->SetTitle("Bin Members");
 
   TCanvas *c1 = new TCanvas("c1","c1",0,400,600,300);
-  ch.Draw("NbOfPMTHits>>hMECount");
+  c1->SetLogy();
+  ch.Draw("NbOfPMTHits>>hNPE");
   
 }
