f94e3233e97d46ae015dcfcba164ed84e929fd6b
rtschmitz <schmitz@ucsb.edu>
2023-08-14 09:18:56 -0700

    Added macro to extract fourvectors from propagation step and format them for the sim. Also updated bar numbering scheme

---
+++ rootMacros/extract_values.C
@@ -0,0 +1,60 @@
+#include <TFile.h>
+#include <TTree.h>
+#include <TVector3.h>
+#include <TLorentzVector.h>
+#include <iostream>
+#include <fstream>
+
+void extract_values() {
+  TString mode="dy";
+  TString path = "/net/cms26/cms26r0/hmei/milliqan/muons/05252023/"+mode+"/postsim_v1_save2m/";
+  TString outputFile = "output_"+mode+"_muons.txt";
+
+  std::ofstream file(outputFile);
+  int nFiles=0;
+  int entryTot=0;
+  for (int i = 1; i <= 100; i++) { // change if the number of files you want to loop over changes
+    TString fileName = Form("output_%d.root", i);
+    std::cout << "Opening file: " << fileName << std::endl;
+    TFile *f = TFile::Open(path + fileName);
+    if (!f) continue;
+    if (f->IsZombie()) {f->Close(); std::cout << "is zombie!" << std::endl; continue;}
+    if ( !f->Get("Events")) {std::cout << "file is null! No event content found" << std::endl; continue;}
+
+    TTree *tree = (TTree *)f->Get("Events"); // Change to the name of your tree
+
+    TVector3 *hit_p_xyz = nullptr;
+    TLorentzVector *hit_p_p4 = nullptr;
+    Float_t weight;
+    Double_t sim_q;
+
+    tree->SetBranchAddress("hit_p_xyz", &hit_p_xyz);
+    tree->SetBranchAddress("hit_p_p4", &hit_p_p4);
+    tree->SetBranchAddress("sim_q", &sim_q);
+    tree->SetBranchAddress("weight", &weight);
+
+    int nEntries = tree->GetEntries();
+
+    for (int j = 0; j < nEntries; j++) {
+      tree->GetEntry(j);
+      double charge = sim_q;
+      double mass = hit_p_p4->M();
+      double X = hit_p_xyz->X();
+      double Y = hit_p_xyz->Y();
+      double Z = hit_p_xyz->Z();
+      double Px = hit_p_p4->Px();
+      double Py = hit_p_p4->Py();
+      double Pz = hit_p_p4->Pz();
+
+      file << charge << " " << mass << " " << X << " " << Y << " " << Z << " " << Px << " " << Py << " " << Pz << " " << weight << std::endl;
+    }
+    std::cout << "File had " << nEntries << " entries" << std::endl;    
+    nFiles++;
+    entryTot+=nEntries;
+    f->Close();
+  }
+
+  file.close();
+  std::cout << "Extraction complete, " << nFiles << " files converted with " << entryTot << " total entries. Results saved to " << outputFile << std::endl;
+}
+
