dc6217d3325010e3dc87103658a6f7dd7f1fc02c
rtschmitz <schmitz@ucsb.edu>
2024-10-02 17:59:41 -0700

    Added MilliQan.cc.gps file to ensure no errors exist when running gps quickstarts

--- formosaBeamSetup.sh
+++ formosaBeamSetup.sh
@@ -2,6 +2,7 @@
 #create local build of sim, configured to generate cosmic muons
 . buildsetup.sh
 cp CMakeLists.txt.default CMakeLists.txt 
+cp MilliQan.cc.gps MilliQan.cc
 cp include/default/* include/
 cp src/default/* src/
 cp src/mqDetectorConstruction.cc.formosa src/mqDetectorConstruction.cc
